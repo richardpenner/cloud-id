593404b431224160c80834226f2ae16283355c3d
richardpenner <richard.penner@shopify.com>
2020-10-21 15:54:16 -0400

    updated azure IP ranges link

--- providers/Azure.js
+++ providers/Azure.js
@@ -1,8 +1,8 @@
 const CIDRMatcher = require("cidr-matcher");
 const fetch = require("node-fetch");
 const fs = require("fs");
-// TODO: this seems to change
-const AZURE_IP_RANGES = "https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20200302.json";
+// TODO: this seems to change. Go here: https://www.microsoft.com/en-us/download/details.aspx?id=56519 then copy the "click here to download manually" link
+const AZURE_IP_RANGES = "https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20201019.json";
 const AZURE_IP_RANGES_LOCAL = "./provider-ips/azure.json";
 
 class Azure {
